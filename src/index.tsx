33a7e49ab19c393f42baf79c20f4ed091c0dac7d
sanjay7178 <saisanjay7660@gmail.com>
2024-07-30 22:26:13 +0530

    Update index.tsx and fetch version code from build.gradle

--- src/index.tsx
+++ src/index.tsx
@@ -24,6 +24,42 @@ app.get('/about.json', cache({ cacheName: 'github-release-info', cacheControl: '
   }
 })
 
+
+async function fetchAndGetVersionCode(url: string): Promise<number | null> {
+  try {
+    // Fetch the content from the URL
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    // Get the text content of the response
+    const gradleContent = await response.text();
+    
+    // Parse the content to get the version code
+    return getVersionCode(gradleContent);
+  } catch (error) {
+    console.error('Error fetching or parsing content:', error);
+    return null;
+  }
+}
+
+function getVersionCode(gradleContent: string): number | null {
+  const lines = gradleContent.split('\n');
+  for (const line of lines) {
+    if (line.trim().startsWith('versionCode')) {
+      const match = line.match(/versionCode\s+(\d+)/);
+      if (match && match[1]) {
+        return parseInt(match[1], 10);
+      }
+    }
+  }
+  return null;
+}
+
+
+
 // Define an async function that fetches and formats the data
 export async function fetchFormattedData(version?: string) {
   let url = `https://api.github.com/repos/${owner}/${repo}/releases/latest`;
@@ -47,13 +83,22 @@ export async function fetchFormattedData(version?: string) {
     }
 
     const data :  any = await response.json();
+    const url2 = 'https://raw.githubusercontent.com/sanjay7178/android-vtop-vitap/master/app/build.gradle';
+
+
+    const versionCode = await fetchAndGetVersionCode(url2).then((versionCode) => versionCode);
+    console.log('versionCode:', versionCode);
+    if (versionCode === null) {
+      throw new Error('Failed to fetch version code');
+      console.error('Failed to fetch version code');
+    }
 
     return {
       downloads: data.assets.reduce((total: any, asset: any) => total + asset.download_count, 0),
       updatedOn: data.published_at,
       releaseNotes: data.body,
       tagName: data.tag_name,
-      versionCode: data.id,
+      versionCode:versionCode,
       authorisedUserAgent: 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.40 Mobile Safari/537.36'
     };
   } catch (error) {
